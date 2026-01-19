0cf8d0d03ae85cf60c82841943cfd20d79cd7c5e
Priyam <chaudharypriyam15@gmail.com>
2026-01-19 10:46:20 +0530

    TopBrainsQuestion6

---
+++ TopBrainsPracticeQuestions/Q6/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+class Program
+{
+    public static void Main(string[] args)
+    {
+        int num1=Convert.ToInt32(Console.ReadLine());
+        int num2=Convert.ToInt32(Console.ReadLine());
+        int num3=Convert.ToInt32(Console.ReadLine());
+        if (num1 > num2)
+        {
+            if(num1>num3) System.Console.WriteLine("Num1 is largest");
+            else System.Console.WriteLine("Num3 is largest");
+        }
+        else
+        {
+            if (num2 > num3) System.Console.WriteLine("Num2 is largest");
+            else System.Console.WriteLine("Num3 is largest");
+        }
+    }
+}
