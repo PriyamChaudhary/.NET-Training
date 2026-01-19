4ecdacc17bb8ca242505766052fe8d673d42b098
Priyam <chaudharypriyam15@gmail.com>
2026-01-19 10:40:57 +0530

    TopBrainsQuestion3

---
+++ TopBrainsPracticeQuestions/Q3/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+class Program
+{
+    public static void Main(string[] args)
+    {
+        int n = Convert.ToInt32(Console.ReadLine());
+        int upto = Convert.ToInt32(Console.ReadLine());
+        int[] row = new int[upto];
+        for(int i = 0; i < upto; i++)
+        {
+            row[i]=n*(i+1);
+        }
+        System.Console.Write("[");
+        for(int i = 0; i < upto; i++)
+        {
+            System.Console.Write(row[i]+",");
+        }
+        System.Console.WriteLine("]");
+    }
+}
