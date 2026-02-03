d263cb9fda53dbf5aee10b9832af8ac792d0198b
Priyam <chaudharypriyam15@gmail.com>
2026-02-03 14:35:06 +0530

    TopBrainsPractice

---
+++ TopBrainsPracticeQuestions/MidpointRounding/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+ 
+
+ class Program
+{
+    public static void Main(string[] args)
+    {
+        double radius =Convert.ToDouble(Console.ReadLine());
+        double area = CalculateCircleArea(radius);
+        Console.WriteLine(area);
+    }
+    public static double CalculateCircleArea(double radius)
+    {
+        double area = Math.PI * radius * radius;
+        return Math.Round(area,2,MidpointRounding.AwayFromZero);
+    }
+}
