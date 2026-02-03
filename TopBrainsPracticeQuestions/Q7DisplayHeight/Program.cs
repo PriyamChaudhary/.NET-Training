920b2ffdea1ad08b31de3242ae88dc7746fd1ed0
Priyam <chaudharypriyam15@gmail.com>
2026-02-03 15:01:08 +0530

    Assignment-03-02-2026

---
+++ TopBrainsPracticeQuestions/Q7DisplayHeight/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace Q7DisplayHeight;
+
+class Program
+{
+    static void Main()
+    {
+        int height=Convert.ToInt32(Console.ReadLine());
+        string category = GetHeightCategory(height);
+        Console.WriteLine(category);
+    }
+
+    static string GetHeightCategory(int height)
+    {
+        if (height < 150)
+            return "Short";
+        else if (height < 180)
+            return "Average";
+        else
+            return "Tall";
+    }
+}
